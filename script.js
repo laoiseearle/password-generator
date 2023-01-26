5a92adef79578e9740dab1510d554fd6be5425f2
Laoise Earle <laoiseearle92@gmail.com>
2023-01-26 23:52:18 +0000

    Create function for generating password

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+// Slider
 const slider = document.getElementById('sliderRange');
 const sliderLabel = document.getElementById('charSlider');
+
+// Password
+const passwordText = document.getElementById('passwordText');
+
+// Checkboxes
+const upperCase = document.getElementById('checkboxUppercase');
+const lowerCase = document.getElementById('checkboxLowercase');
+const numbers = document.getElementById('checkboxNumbers');
+const symbols = document.getElementById('checkboxSymbols');
 
 slider.addEventListener('input', function () {
   const x = slider.value * 5;
@@ -7,3 +17,34 @@ slider.addEventListener('input', function () {
   slider.style.background = color;
   sliderLabel.innerText = slider.value;
 });
+
+const randomPassword = max => {
+  let charSelection = '';
+  let result = '';
+
+  if (upperCase.checked) charSelection += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  if (lowerCase.checked) charSelection += 'abcdefghijklmnopqrstuvwxyz';
+  if (numbers.checked) charSelection += '0123456789';
+  if (symbols.checked) charSelection += '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';
+
+  for (let i = 0; i < max; i++) {
+    result += charSelection.charAt(
+      Math.floor(Math.random() * charSelection.length)
+    );
+  }
+
+  return result;
+};
+
+const generatePassword = () => {
+  let charLength = sliderLabel.innerText;
+
+  if (charLength === '0') {
+    passwordText.innerText = 'P4$5W0rD!';
+    passwordText.style.opacity = ' 0.25';
+    return;
+  }
+
+  passwordText.style.opacity = '1';
+  passwordText.innerText = randomPassword(charLength);
+};
