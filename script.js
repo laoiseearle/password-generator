5490a2a53dfd22bbbdc9241a3ee6fb322207e8ce
Laoise Earle <laoiseearle92@gmail.com>
2023-01-27 22:38:06 +0000

    Create functions for strength rating

--- script.js
+++ script.js
@@ -12,12 +12,76 @@ const lowerCase = document.getElementById('checkboxLowercase');
 const numbers = document.getElementById('checkboxNumbers');
 const symbols = document.getElementById('checkboxSymbols');
 
+// Strength
+const strengthText = document.getElementById('strengthRatingText');
+const strengthBars = document.getElementsByClassName('strength-bar');
+
 slider.addEventListener('input', function () {
   const x = slider.value * 5;
   const color = `linear-gradient(90deg, #a4ffaf ${x}%, #18171f ${x}%)`;
   slider.style.background = color;
   sliderLabel.innerText = slider.value;
 });
+
+const setBarColor = (color, maxBars) => {
+  for (let i = 0; i < 4; i++) {
+    if (i < maxBars) {
+      strengthBars[i].style.backgroundColor = color;
+      strengthBars[i].style.border = 'none';
+    } else {
+      strengthBars[i].style.backgroundColor = 'transparent';
+      strengthBars[i].style.border = '2px solid white';
+    }
+  }
+};
+
+const getStrengthScore = (charLength, checkboxes) => {
+  let strength = 0;
+
+  if (charLength === 0) {
+    return 0;
+  }
+
+  if (charLength >= 8) {
+    strength += 3;
+  }
+
+  if (charLength >= 11) {
+    strength += 1;
+  }
+
+  if (charLength >= 14) {
+    strength += 2;
+  }
+
+  // Each checkbox adds 1 strength
+  strength += checkboxes.filter(checkbox => checkbox.checked).length;
+
+  return strength;
+};
+
+const setStrengthBars = () => {
+  const strength = getStrengthScore(sliderLabel.innerText, [
+    upperCase,
+    lowerCase,
+    numbers,
+    symbols,
+  ]);
+
+  if (strength >= 8) {
+    strengthText.innerText = 'Strong';
+    setBarColor('#a4ffaf', 4);
+  } else if (strength >= 6) {
+    strengthText.innerText = 'Medium';
+    setBarColor('#f8cd65', 3);
+  } else if (strength >= 4 && sliderLabel.innerText >= 4) {
+    strengthText.innerText = 'Weak';
+    setBarColor('#fb7c58', 2);
+  } else {
+    strengthText.innerText = 'Too Weak!';
+    setBarColor('#f64a4a', 1);
+  }
+};
 
 const randomPassword = max => {
   let charSelection = '';
@@ -44,14 +108,16 @@ const generatePassword = () => {
   if (charLength === '0') {
     passwordText.innerText = 'P4$5W0rD!';
     passwordText.style.opacity = ' 0.25';
-    return;
+  } else {
+    passwordText.style.opacity = '1';
+    passwordText.innerText = randomPassword(charLength);
   }
-
-  passwordText.style.opacity = '1';
-  passwordText.innerText = randomPassword(charLength);
+  setStrengthBars();
 };
 
 const copyTextToClipboard = () => {
   navigator.clipboard.writeText(passwordText.innerText);
   copyText.classList.add('show-text');
 };
+
+setStrengthBars();
